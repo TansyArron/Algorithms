872055a6fae8822b1a523f49b7e13e30c2f7ab50
Tansy Arron <tansy.arron@gmail.com>
2015-03-17 14:34:22 -0400

    cleaned up quicksort

--- quickSort.py
+++ quickSort.py
@@ -2,11 +2,9 @@ arr100 = [line.strip() for line in open("100.txt", 'r')]
 arr100 = [int(i) for i in arr100]
 
 def quicksort(unsorted_list, lower=0, upper=None):
-	print('current list: {}'.format(unsorted_list[lower:upper]))
 	if upper == None:
 		upper = len(unsorted_list)
 	if upper - lower < 2:
-		print('STOPPING BRANCH')
 		return
 	else:
 		pivot = unsorted_list[lower]
@@ -22,10 +20,6 @@ def quicksort(unsorted_list, lower=0, upper=None):
 		temp = unsorted_list[first_larger_index - 1]
 		unsorted_list[first_larger_index - 1] = pivot
 		unsorted_list[lower] = temp
-		print('moving pivot {} to correct location: {}'.format(pivot, unsorted_list))
-		print('left half:', unsorted_list[lower:first_larger_index - 1])
-		print('right half:', unsorted_list[first_larger_index:upper])
-		#unsorted_list[:first_larger_index - 1] = quicksort(unsorted_list[:first_larger_index - 1])
 		quicksort(unsorted_list, lower, first_larger_index-1)
 		quicksort(unsorted_list, first_larger_index, upper)
 	
