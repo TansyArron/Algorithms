404875e6d960f3b4642e5587613577a19be02ca3
Tansy Arron <tansy.arron@gmail.com>
2015-04-27 22:54:01 -0400

    more stuff

--- hashtable.py
+++ hashtable.py
@@ -1,34 +1,3 @@
-class Node():
-	def __init__(self, key):
-		self.previous = None
-		self.next = None
-
-
-class Order():
-	def __init__(self):
-		self.start = None
-		self.end = None
-
-
-	def add(self, key):
-		node = Node(key)
-		if self.start == None:
-			self.start = node
-			self.end = node
-			return node
-		node.previous = self.end
-		node.previous.next = node
-		self.end = node
-		return node
-
-
-	def remove(self, node):
-		if node.previous:
-			node.previous.next = node.next
-		if node.next:
-			node.next.previous = node.previous
-		if not node.next:
-			self.end = node.previous
 
 
 class Hashtable():
@@ -36,7 +5,6 @@ class Hashtable():
 		self.num_keys = 0
 		self.length = length
 		self.table = [None] * self.length	
-		self.order = Order()
 
 
 	def add(self, key, hash_num=None):
@@ -45,24 +13,23 @@ class Hashtable():
 		index = hash_num % self.length
 		while True:
 			if self.table[index] is None or self.table[index] == 'removed item':
-				order_info = self.order.add(key)
-				self.table[index] = (key, hash_num, order_info)
+				self.table[index] = (key, hash_num)
+				print(self.table)
 				self.num_keys += 1
 				if self.num_keys > self.length * .6:
+					print('RESIZE')
 					self.resize()
 				return
 			else:
+				print("collision at index {}".format(index))
 				index = (index + 1) % self.length
 
 
 	def remove(self, key):
 		entry, index = self.find(key)
-		self.order.remove(self.table[index][2])
 		self.table[index] = 'removed item'
 		self.num_keys -= 1
-		print(self.table)
-		pass
-
+		
 
 	def find(self, key):
 		hash_num = abs(hash(key))
@@ -79,17 +46,27 @@ class Hashtable():
 		new_table = Hashtable(self.length*2)
 		for i in range(self.length):
 			if self.table[i] is not None and self.table[i] != 'removed item':
-				key, hash_num, order_info = self.table[i]
+				key, hash_num = self.table[i]
 				new_table.add(key, hash_num)
+		self.length = new_table.length
 		self.table = new_table.table
 		return
 
-
-	def iterate_in_order(self):
-		current = self.order.start
-		ordered_list = []
-		while current != None:
-			ordered_list.append(current.key)
-			current = current.next
-		return ordered_list
-
+table = Hashtable()
+table.add('first')
+table.add('second')
+table.find('first')
+table.remove('first')
+table.add('third')
+print(table.table)
+table.add('fourth')
+print(table.length)
+table.add('fifth')
+table.add('sixth')
+table.add('seventh')
+table.add('eighth')
+table.add('ninth')
+table.add('tenth')
+print(table.table)
+print(table.num_keys)
+print(table.length)
